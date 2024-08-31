84d21f98d75ba1e9a6e013a0d445fd72ff05162f
Adithya Shankar <adithya7shankar@gmail.com>
2024-08-31 03:30:29 +0000

    algo 1

--- app/Zalgo.py
+++ app/Zalgo.py
@@ -3,4 +3,44 @@ def getzarr(string, z):
 
     l, r, k = 0, 0, 0
     for i in range(1,n):
-        l,r == i,i 
+        l,r == i,i
+
+        while r < n and string[r-l] == string[r]:
+            r += 1
+        z[i] = r - l 
+        r-=1
+    else:
+
+        k = i -1
+
+        if z[k] < r - i + 1:
+            z[i] = z[k]
+
+        else:
+
+            l = i
+            while r < n and string[r -l] == string[r]:
+                r += 1
+
+            z[i] = r -l 
+            r -= 1
+
+def search( text, pattern):
+
+    concat = pattern + "$" + text
+    l = len(concat)
+
+    z = [0] * l 
+
+    getzarr(concat, z)
+
+    for i in range(l):
+
+        if z[i] == len(pattern):
+            print("pattern found at index", i -len(pattern)-1)
+
+
+if __name__ == "__main__":
+    text = "ajdjdfh"
+    pattern = "af"
+    search(text, pattern)
